333f7b06d124ac115570909e7b1855d646862def
luka <lukakhvichia@gmail.com>
2022-03-29 14:03:24 +0400

    change massage props and maps

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,7 @@ import { NavLink, outlet } from 'react-router-dom';
 import cl from './Dialogs.module.css';
 
 
-const DialogItems =(props)=> {
+const DialogItems = (props) => {
     let Path = "/Dialogs/" + props.id;
 
     return (
@@ -14,23 +14,42 @@ const DialogItems =(props)=> {
 }
 
 const Massage = (props) => {
-    return  <div className={cl.massage}>{props.massage}</div>
+    return <div className={cl.massage}>{props.massage}</div>
 }
 
 const Dialogs = (props) => {
-    return(
+
+    let dialogsData = [
+        { id: 'luka', name: 'luka' },
+        { id: 'zaza', name: 'zaza' },
+        { id: 'dato', name: 'dato' },
+        { id: 'dima', name: 'dima' },
+        { id: 'achiko', name: 'achiko' }
+    ]
+
+    let DialogElements = dialogsData
+        .map(d => <DialogItems name={d.name} id={d.id} />);
+
+
+
+    let massages = [
+        { id: '1', massage: 'how are you' },
+        { id: '2', massage: 'im good' },
+        { id: '3', massage: 'and you' },
+        { id: '4', massage: 'im fine' }
+    ]
+
+    let MassageElements = massages
+        .map(m => <Massage massage={m.massage} />);
+
+
+    return (
         <div className={cl.dialogs}>
             <div className={cl.dialogs_items}>
-                <DialogItems name="luka" id="luka" />
-                <DialogItems name="zaza" id="zaza" />
-                <DialogItems name="dato" id="dato" />
-                <DialogItems name="dima" id="dima" />
-                <DialogItems name="achiko" id="achiko" />
+                {DialogElements}
             </div>
             <div className={cl.massages}>
-                <Massage massage = "how are you" />
-                <Massage massage = "im ok" />
-                <Massage massage = "great" />
+                {MassageElements}
             </div>
         </div>
     )
--- src/components/Profile/Myposts/Myposts.jsx
+++ src/components/Profile/Myposts/Myposts.jsx
@@ -3,13 +3,18 @@ import  cl from './Myposts.module.css'
 import Post from './Post/Post';
 
 const Myposts = (props) => {
+
+	let postsData = [
+		{id:1, post: 'hi', like: 10},
+		{id:2, post: 'how are you', like: 20}
+	]
 	return (
 			<div className={cl.posts}>
 				<div className='tcenter'>
 					my post
 				</div>
-				<Post  massage="hi." likeCount='10'/>
-				<Post  massage="how are you?" likeCount='20'/>
+				<Post  massage={postsData[0].post} likeCount={postsData[0].like}/>
+				<Post  massage={postsData[1].post} likeCount={postsData[1].like}/>
 			</div>
 	)
 };
