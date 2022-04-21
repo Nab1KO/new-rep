002c28d3374a45327fb6289d08d596310bb058f7
luka <lukakhvichia@gmail.com>
2022-04-21 23:34:33 +0400

    set users reducer

--- src/components/News/NewsContainer.jsx
+++ src/components/News/NewsContainer.jsx
@@ -1,23 +1,48 @@
+import { connect } from 'react-redux';
 import { onNewsMassageChangeCreator } from '../../Redux/News-reducer';
 import { onNewsMassageSandCreator } from '../../Redux/News-reducer';
 import News from './News';
 
-const NewsContainer = (props) => {
-    let state = props.store.getState().newsPage;
+// const NewsContainer = (props) => {
+//     let state = props.store.getState().newsPage;
 
-    let onNewsMassageChange = (body) => {
-        let action = onNewsMassageChangeCreator(body);
-        props.store.dispatch(action);
-    }
+//     let onNewsMassageChange = (body) => {
+//         let action = onNewsMassageChangeCreator(body);
+//         props.store.dispatch(action);
+//     }
 
-    let onNewsMassageSend = () => {
-        props.store.dispatch(onNewsMassageSandCreator());
+//     let onNewsMassageSend = () => {
+//         props.store.dispatch(onNewsMassageSandCreator());
+//     }
+//     return(
+//         <News onNewsMassageSend = {onNewsMassageSend}
+//                 onNewsMassageChange = {onNewsMassageChange}
+//                 newsPage = {state} />
+//     )
+// }
+
+
+
+let mapStateToProps = (state) => ({
+    newsMassagesData: state.newsPage.newsMassagesData,
+    newsMassageBody: state.newsPage.newsMassageBody
+});
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        onNewsMassageChange: (body) => {
+            let action = onNewsMassageChangeCreator(body);
+            dispatch(action);
+        },
+        onNewsMassageSend: () => {
+            dispatch(onNewsMassageSandCreator())
+        }
     }
-    return(
-        <News onNewsMassageSend = {onNewsMassageSend}
-                onNewsMassageChange = {onNewsMassageChange}
-                newsPage = {state} />
-    )
 }
+
+
+    
+
+const NewsContainer = connect(mapStateToProps, mapDispatchToProps)(News);
 
 export default NewsContainer;
